67719f413b99175cbbc82187660e12ea2d2861ef
KevinW <bird831205@hotmail.com>
2020-01-23 08:24:48 -0500

    config firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBkStqCyeNj-2RbAtckAAD8wvG2VPMGbO8",
+    authDomain: "demoangularfirebase-9a5b9.firebaseapp.com",
+    databaseURL: "https://demoangularfirebase-9a5b9.firebaseio.com",
+    projectId: "demoangularfirebase-9a5b9",
+    storageBucket: "demoangularfirebase-9a5b9.appspot.com",
+    messagingSenderId: "399806563162",
+    appId: "1:399806563162:web:dd1edfb4556419ad076963",
+    measurementId: "G-26NZQ4BJ53"
+  }
 };
