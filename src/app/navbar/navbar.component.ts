810c7ef0a34fc38ed9dce83c40923786a748febf
KevinW <bird831205@hotmail.com>
2020-01-25 19:03:47 -0500

    refactor navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,13 +10,10 @@ import { Observable } from 'rxjs';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  user$ : Observable<firebase.User>;
-
-  constructor(private afAuth: AngularFireAuth) {
-    this.user$  = afAuth.authState;
+  constructor(public auth: AuthService) {
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
 }
