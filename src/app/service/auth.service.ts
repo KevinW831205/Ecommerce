c3e970426588820b0fa669cd725cf5f719c497a3
KevinW <bird831205@hotmail.com>
2020-01-25 20:43:49 -0500

    redirect after auths

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { LoginComponent } from '../login/login.component';
 import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,22 @@ import { Observable } from 'rxjs';
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth ) {
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private router: Router) {
     this.user$ = afAuth.authState;
   }
 
-  login(){
+  login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+
   }
 
-  logout(){
-    this.afAuth.auth.signOut();
+  logout() {
+    this.afAuth.auth.signOut().then(() => {
+      this.router.navigate(['/'])
+    });
+
   }
 }
