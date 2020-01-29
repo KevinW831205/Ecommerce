d230e4c35ce1a47f2b69a294334bb9bdfe9deca4
KevinW <bird831205@hotmail.com>
2020-01-29 13:11:03 -0500

    creating a table of products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/service/product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductsComponent implements OnInit {
 
-  constructor() { }
+  products$: Observable<any>;
+
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.getAll()
+  }
 
   ngOnInit() {
+    this.products$.subscribe(res=>{
+      console.log(res);
+    })
   }
 
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,21 @@ export class ProductService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create(product){
+  getAll(): Observable<any> {
+    return this.db.list('/products').snapshotChanges()
+      .pipe(
+        map(category => {
+          return category.map(c => {
+            const key = c.payload.key;
+            const data = c.payload.val();
+            return { key, data };
+          })
+        })
+      );
+    ;
+  }
+
+  create(product) {
     return this.db.list('/products').push(product);
   }
 }
