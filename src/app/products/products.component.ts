4aff08e1bd32474987a593cae27bbbc750c8d3b5
KevinW <bird831205@hotmail.com>
2020-01-30 08:31:23 -0500

    simplifying some code

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -4,6 +4,7 @@ import { CategoryService } from '../service/category.service';
 import { ActivatedRoute } from '@angular/router';
 import { FirebaseData } from '../models/FirebaseData';
 import { Product } from '../models/Product';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -17,18 +18,34 @@ export class ProductsComponent {
   category: string;
 
   constructor(private productService: ProductService, categoryService: CategoryService, route: ActivatedRoute) {
+
     productService.getAll()
-      .subscribe(
-        products => {
+      .pipe(
+        switchMap(products => {
           this.products = products;
-          route.queryParamMap.subscribe(params => {
-            this.category = params.get('category');
-            this.filteredProducts = this.category ?
-              this.products.filter(p => p.data.category === this.category) :
-              this.products
-          })
-        }
-      );
+          return route.queryParamMap;
+        })
+      )
+      .subscribe(params => {
+        this.category = params.get('category');
+        this.filteredProducts = this.category ?
+          this.products.filter(p => p.data.category === this.category) :
+          this.products;
+      })
+
+    // .subscribe(
+    //   products => {
+    //     this.products = products;
+    //     route.queryParamMap.subscribe(params => {
+    //       this.category = params.get('category');
+    // this.filteredProducts = this.category ?
+    //   this.products.filter(p => p.data.category === this.category) :
+    //   this.products
+    //     })
+    //   }
+    // );
+
+
     this.categories$ = categoryService.getAll();
 
   }
