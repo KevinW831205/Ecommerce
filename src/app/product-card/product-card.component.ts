9ca016364b5e8d1c825683de45a1e5b377489de3
KevinW <bird831205@hotmail.com>
2020-01-30 13:11:04 -0500

    displays item in shopping cart

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -11,11 +11,20 @@ import { FirebaseData } from '../models/FirebaseData';
 export class ProductCardComponent {
   @Input('product') product: FirebaseData<Product> = null;
   @Input('show-actions') showActions = false;
+  @Input('shopping-cart') shoppingCart;
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
   addToCart(product: FirebaseData<Product>) {
     this.shoppingCartService.addToCart(product);
+  }
+
+  getQuantity(){
+    if(!this.shoppingCart){
+      return 0;
+    }
+    let item =  this.shoppingCart.items[this.product.key];
+    return item ? item.quantity: 0;
   }
 
 }
