e953642302da9e01a456b34a216afe586c9ff6b5
KevinW <bird831205@hotmail.com>
2020-01-30 11:30:22 -0500

    extracting shopping cart logic to service

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -14,14 +14,6 @@ export class ProductCardComponent {
   constructor(private shoppingCartService: ShoppingCartService) { }
 
   addToCart(product: Product) {
-    let cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      this.shoppingCartService.create().then(
-        result => { localStorage.setItem('cartId', result.key) }
-      );
-    } else {
-      
-    }
 
   }
 
--- src/app/service/shopping-cart.service.ts
+++ src/app/service/shopping-cart.service.ts
@@ -8,9 +8,26 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create() {
+  private create() {
     return this.db.list('/shopping-carts').push({
-      dateCreated:new Date().getTime()
+      dateCreated: new Date().getTime()
     })
+  }
+
+  private getCart(cartId: string) {
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private getOrCreateCart() {
+    let cartId = localStorage.getItem('cartId');
+    if (!cartId) {
+      this.create().then(
+        result => {
+          localStorage.setItem('cartId', result.key)
+          return this.getCart(result.key);
+        });
+    } else {
+      return this.getCart(cartId);
+    }
   }
 }
