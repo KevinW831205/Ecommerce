0780990da4f522fd6389b280935db51bcc5d5c78
KevinW <bird831205@hotmail.com>
2020-01-30 12:03:02 -0500

    can add to shopping cart

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../models/Product';
 import { ShoppingCartService } from '../service/shopping-cart.service';
+import { FirebaseData } from '../models/FirebaseData';
 
 @Component({
   selector: 'app-product-card',
@@ -8,13 +9,13 @@ import { ShoppingCartService } from '../service/shopping-cart.service';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent {
-  @Input('product') product = null;
+  @Input('product') product: FirebaseData<Product> = null;
   @Input('show-actions') showActions = false;
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
-  addToCart(product: Product) {
-
+  addToCart(product: FirebaseData<Product>) {
+    this.shoppingCartService.addToCart(product);
   }
 
 }
--- src/app/service/shopping-cart.service.ts
+++ src/app/service/shopping-cart.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Product } from '../models/Product';
+import { FirebaseData } from '../models/FirebaseData';
+import { take, map } from 'rxjs/operators'
+import { Item } from '../models/Item';
 
 @Injectable({
   providedIn: 'root'
@@ -18,19 +22,33 @@ export class ShoppingCartService {
     return this.db.object('/shopping-carts/' + cartId);
   }
 
-  private async getOrCreateCart() {
+  private async getOrCreateCartId() {
     let cartId = localStorage.getItem('cartId');
     if (!cartId) {
       let result = await this.create();
       localStorage.setItem('cartId', result.key)
-      return this.getCart(result.key);
+      return result.key;
       // this.create().then(
       //   result => {
       // localStorage.setItem('cartId', result.key)
       // return this.getCart(result.key);
       //   });
-    } else {
-      return this.getCart(cartId);
     }
+    return cartId;
+  }
+
+  async addToCart(product: FirebaseData<Product>) {
+    let cartId = await this.getOrCreateCartId();
+    let item$ = this.db.object<Item>('shopping-carts/' + cartId + '/items/' + product.key);
+    item$.snapshotChanges().pipe(
+      take(1)
+    ).subscribe(item => {
+      console.log("payload val", item.payload.val());
+      if (item.payload.val()) {
+        item$.update({ quantity: item.payload.val().quantity + 1 })
+      } else {
+        item$.set({ product: product.data, quantity: 1 })
+      }
+    })
   }
 }
