7c458ebcacf2e1a6590e4e5c1a2acd204793818b
KevinW <bird831205@hotmail.com>
2020-01-30 21:01:08 -0500

    more refactoring of model to fit service

--- src/app/models/ShoppingCart.ts
+++ src/app/models/ShoppingCart.ts
@@ -2,18 +2,18 @@ import { Item } from './Item';
 
 export class ShoppingCart {
 
-    items: Item[] = [];
-    constructor(public itemsMap: { [key: string]: Item }) {
-        for (let productId in itemsMap) {
-            let item = itemsMap[productId]
-            this.items.push(new Item(item.product, item.quantity))
+    itemsArr: Item[] = [];
+    constructor(public items: { [key: string]: Item }) {
+        for (let productId in items) {
+            let item = items[productId]
+            this.itemsArr.push(new Item(item.product, item.quantity))
         }
     }
 
 
     get totalItemCount(): number {
         let count = 0;
-        for (let productId in this.itemsMap) {
+        for (let productId in this.items) {
             count += this.items[productId].quantity;
         }
         return count;
