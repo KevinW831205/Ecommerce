b8b970a92dd7b1e4656625f43f645e9da8dc8c26
KevinW <bird831205@hotmail.com>
2020-02-01 20:57:27 -0500

    modifying the order service

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from 'src/app/service/order.service';
+import { Observable } from 'rxjs';
+import { Order } from 'src/app/models/Order';
 
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
   styleUrls: ['./admin-orders.component.css']
 })
-export class AdminOrdersComponent implements OnInit {
+export class AdminOrdersComponent {
+  orders$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private orderService: OrderService) {
+    this.orders$=orderService.getOrders();
   }
 
 }
--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Order } from '../models/Order';
 import { ShoppingCartService } from './shopping-cart.service';
-import { BehaviorSubject } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+import { FirebaseData } from '../models/FirebaseData';
 
 @Injectable({
   providedIn: 'root'
@@ -18,12 +19,28 @@ export class OrderService {
     return result;
   }
 
-  getOrders() {
-    return this.db.list('/orders');
+  getOrders(): Observable<FirebaseData<Order>[]> {
+    return this.db.list<Order>('/orders').snapshotChanges().pipe(
+      map(orders => {
+        return orders.map(o => {
+          const key = o.key;
+          const data = o.payload.val();
+          return new FirebaseData(key, data);
+        })
+      })
+    );
   }
 
   getOrdersByUser(userId: string) {
-    this.db.list('/orders', ref => ref.equalTo(userId));
+    this.db.list('/orders', ref => ref.equalTo(userId)).snapshotChanges().pipe(
+      map(orders => {
+        return orders.map(o => {
+          const key = o.key;
+          const data = o.payload.val();
+          return new FirebaseData(key, data);
+        })
+      })
+    );
   }
 
   private storeOrder(order: Order) {
