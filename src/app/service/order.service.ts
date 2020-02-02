716360414f3f3f93f87b43413612a4321450576a
KevinW <bird831205@hotmail.com>
2020-02-01 20:42:11 -0500

    get in orders service

--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Order } from '../models/Order';
 import { ShoppingCartService } from './shopping-cart.service';
+import { BehaviorSubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,18 @@ export class OrderService {
 
   constructor(private db: AngularFireDatabase, private shoppingCartService: ShoppingCartService) { }
 
-  async placeOrder(order: Order){
+  async placeOrder(order: Order) {
     let result = await this.storeOrder(order);
     this.shoppingCartService.clearCart();
     return result;
+  }
+
+  getOrders() {
+    return this.db.list('/orders');
+  }
+
+  getOrdersByUser(userId: string) {
+    this.db.list('/orders', ref => ref.equalTo(userId));
   }
 
   private storeOrder(order: Order) {
