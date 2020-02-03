e834e83c0f263f3cf77da3c956f85d0af9e0110d
KevinW <bird831205@hotmail.com>
2020-02-02 22:32:44 -0500

    order details page

--- src/app/shopping/components/order-details/order-details.component.ts
+++ src/app/shopping/components/order-details/order-details.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { OrderService } from 'shared/services/order.service';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { FirebaseData } from 'shared/models/FirebaseData';
 import { Order } from 'shared/models/Order';
+import { OrderService } from 'shared/services/order.service';
+import { Item } from 'shared/models/Item';
 
 @Component({
   selector: 'app-order-details',
@@ -11,17 +13,27 @@ import { Order } from 'shared/models/Order';
   styleUrls: ['./order-details.component.css']
 })
 export class OrderDetailsComponent implements OnInit {
-  order$: Observable<FirebaseData<Order>>
+  order: FirebaseData<Order>;
+  orderItems: Item[];
+  subscription: Subscription;
+  total=0;
 
   constructor(private route: ActivatedRoute, private orderService: OrderService) {
 
   }
 
   ngOnInit() {
-    console.log(this.route.snapshot.paramMap.get('id'))
-    this.order$ = this.orderService.getOrderById(this.route.snapshot.paramMap.get('id'))
-    this.order$.subscribe(s=>{
-      console.log(s)
+    let order$ = this.orderService.getOrderById(this.route.snapshot.paramMap.get('id'))
+    order$.pipe(
+      take(1)
+    ).subscribe(order => {
+      this.order = order;
+      this.orderItems = order.data.items.map(i => {
+        return new Item(i);
+      })
+      this.orderItems.forEach(i => {
+        this.total += i.totalPrice;
+      })
     })
   }
 
